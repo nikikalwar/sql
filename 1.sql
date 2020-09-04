d9cc30674c92e721dac9a3fa56747f10dac30aa0
nikikalwar <nikikumar27@gmail.com>
2020-09-05 04:44:53 +0530

    module 1 data camp

--- 1.sql
+++ 1.sql
@@ -5,3 +5,24 @@ FROM film
 -- Filter for films prior to 2006
 where release_year<2006;
 
+
+SELECT payment_date,
+  EXTRACT(day from payment_date) AS payment_day 
+FROM payment;
+
+SELECT payment_date,
+  EXTRACT(year from payment_date) AS payment_year 
+FROM payment;
+
+SELECT payment_date,
+  EXTRACT (hour from payment_date )AS payment_hour 
+FROM payment;
+
+SELECT active, 
+       count(payment_id) AS num_transactions, 
+       avg(amount) AS avg_amount, 
+       sum(amount) AS total_amount
+FROM payment AS p
+INNER JOIN customer AS c
+  ON p.customer_id = c.customer_id
+GROUP BY active;
