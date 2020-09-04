c5f383a3664220acc6be3348979ea79c76d389d1
nikikalwar <nikikumar27@gmail.com>
2020-09-05 05:05:33 +0530

    module 1 remaining problems committed

--- 1.sql
+++ 1.sql
@@ -26,3 +26,12 @@ FROM payment AS p
 INNER JOIN customer AS c
   ON p.customer_id = c.customer_id
 GROUP BY active;
+
+SELECT name, 
+	STRING_AGG(title,',') AS film_titles
+FROM film AS f
+INNER JOIN language AS l
+  ON f.language_id = l.language_id
+WHERE release_year=2010
+  AND rating='G'
+GROUP BY name;
